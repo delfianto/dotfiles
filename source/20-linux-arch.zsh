0897091cdcda55c11a0d776abc5086af3506f817
Dwi Elfianto <dwi.elfianto@gmail.com>
2019-07-20 14:41:46 +0700

    Update wrapper function for yay

--- source/20-linux-arch.zsh
+++ source/20-linux-arch.zsh
@@ -23,42 +23,48 @@ fn.source '/usr/share/LS_COLORS/dircolors.sh'
 
 # Wrapper function for package manager
 pkg() {
-if (( ${+commands[yay]} )); then
-    echo -n ''
-  else
+  if (( !${+commands[yay]} )); then
     sudo pacman --noconfirm -S yay
   fi
 
   case ${1} in
-  'chk')
+  'logs')
+    cat /var/log/pacman.log
+    ;;
+  'check')
     yay "${@:2}"
     ;;
-  'log')
-    cat /var/log/pacman.log
+  'cache')
+    echo 'y' | yay -Sc
+    ;;
+  'clean')
+    if [[ ! -z $(pkg orphans) ]]; then
+      tput setaf 1 #
+      echo 'WARNING: Removing all unneeded dependencies...'
+      tput sgr0
+
+      yay -Yc
+    else
+      echo 'No unneeded dependencies in the system.'
+    fi
     ;;
   'deps')
     pactree -d 1 "${@:2}"
     ;;
+  'file')
+    yay -Qlq "${@:2}"
+    ;;
   'find')
     yay -Ss "${@:2}"
     ;;
-  'files')
-    yay -Qlq "${@:2}"
-    ;;
-  'clean')
-    echo 'y' | yay -Sc
-    ;;
-  'l' | 'ls' | 'list')
-    [[ -z "${2}" ]] && yay -Q || yay -Qs ${@:2}
-    ;;
-  'n' | 'nf' | 'info')
+  'info')
     yay -Qi "${@:2}"
     ;;
-  'o' | 'or' | 'orphans')
+  'orphans')
     yay -Qdtq
     ;;
-  'c' | 'cl' | 'cleanup')
-    pkg rm $(pkg orphans)
+  'l' | 'ls' | 'list')
+    yay -Qs ${@:2}
     ;;
   'i' | 'in' | 'install')
     yay -Sy "${@:2}"
@@ -69,30 +75,8 @@ if (( ${+commands[yay]} )); then
   'u' | 'up' | 'update')
     yay -Syu
     ;;
-  'purge')
-    tput setaf 1 # this command could bork the system, use with care
-    echo 'WARNING: Removing package with all of its unneeded dependencies'
-    echo 'WARNING: THIS OPERATION CAN BE DESTRUCTIVE'
-    tput sgr0
-
-    yay -Rs "${@:2}"
-    ;;
-  'h' | 'help')
-    echo "Usage: ${FUNCNAME[0]} <operation> [...]"
-    echo "operation:"
-    echo "  - chk"
-    echo "  - log"
-    echo "  - deps"
-    echo "  - find"
-    echo "  - files"
-    echo "  - purge"
-    echo "  - l | ls | list"
-    echo "  - n | in | info"
-    echo "  - o | or | orphans"
-    echo "  - c | cl | cleanup"
-    echo "  - i | in | install"
-    echo "  - r | rm | remove"
-    echo "  - u | up | update"
+  'd' | 'db' | 'database' )
+    yay -Syy
     ;;
   *)
     yay "${@:1}"
