48fc3674f77cae2d7a304185e11f275adf5a23a2
Dwi Elfianto <dwi.elfianto@gmail.com>
2025-05-25 18:29:56 +0700

    Enhance logging functions

--- utils/dot_linker.rb
+++ utils/dot_linker.rb
@@ -39,37 +39,72 @@ class DotLinker
       @log.warn("--- No actual linking will be performed ---")
     end
 
-    @log.info "Dotfiles path: #{short_log(@dotfiles_dir)}"
+    mlog("Dotfiles path: {path}", { path: @dotfiles_dir })
   end
 
   def process_modules(mod_names)
-    @log.info "Requested modules: #{mod_names.join(', ')}"
+    mlog("Requested modules: #{mod_names.join(', ')}")
     padding = pad_len(mod_names)
 
     mod_names.each do |mod_name|
-      mod_text = "#{MOD_LOGS}#{mod_name.upcase}"
-      @mod_log = "[#{mod_text.ljust(padding)}]"
-
+      @mod_log = "[#{MOD_LOGS}#{mod_name.upcase.ljust(padding)}]" # Set module log prefix here
       mod_dir = File.join(@dotfiles_dir, mod_name)
-      mod_dir_log = short_log(mod_dir)
 
       unless Dir.exist?(mod_dir)
-        @log.warn "#{@mod_log} Module path does not exist: #{mod_dir_log}"
+        mlog(:warn, "Module path does not exist: {path}", { path: mod_dir })
         next
       end
 
-      @log.info "#{@mod_log} Module name: #{mod_name}"
-      @log.info "#{@mod_log} Module path: #{mod_dir_log}"
+      mlog("Module name: #{mod_name}")
+      mlog("Module path: {path}", { path: mod_dir })
 
       maps = map_module_path(mod_name)
       maps.each do |dest_path, link_path|
-        @log.info "#{@mod_log} --- ENTRY ---"
+        mlog("--- PROCESS ---")
         create_symlink(dest_path, link_path)
       end
     end
+    @mod_log = "" # Reset module log prefix after processing
   end
 
   private # Make methods below this line private
+
+  # Helper for module-specific logging. Automatically prepends @mod_log and formats
+  # paths using short_log. Use placeholders like {path}, {link}, {target}, etc.
+  # in the message string, and provide a hash of path keys to values.
+  # Accepts an optional `exception:` keyword argument to log exceptions.
+  #
+  # Defaults to :info level if the first argument is not a symbol specifying the level,
+  # unless an exception is present, which forces the level to :error.
+  def mlog(*args, exception: nil)
+    level = :info # Start with default info
+
+    if args.first.is_a?(Symbol)
+      # Take the symbol as the level if provided
+      level = args.shift
+    end
+
+    message = args.shift     # The next argument is the message
+    paths = args.shift || {} # The last argument is the paths hash, or default {}
+
+    # If an exception is provided, override the level to :error
+    level = :error if exception
+
+    # Proceed with formatting and logging using level, message, and paths
+    formatted_message = message.to_s.dup # Ensure message is a string
+
+    paths.each do |key, path|
+      formatted_message.gsub!(/\{#{key}\}/, short_log(path))
+    end
+
+    prefixed_message = "#{@mod_log} #{formatted_message}"
+
+    if exception
+      LoggerConfig.log_exception(@log, prefixed_message, exception)
+    else
+      @log.send(level, prefixed_message)
+    end
+  end
 
   def pad_len(mod_names)
     longest = mod_names.max_by(&:length)
@@ -97,7 +132,7 @@ class DotLinker
     # Ensure config_data[MOD_INFO] is not nil before calling reduce
     paths_to_process = config_data[MOD_INFO]
     unless paths_to_process
-      @log.warn "#{@mod_log} #{mod_name}: No data found for MOD_INFO key in YAML."
+      mlog(:warn, "#{mod_name}: No data found for #{MOD_INFO} key in YAML.")
       return {}.freeze
     end
 
@@ -109,13 +144,13 @@ class DotLinker
       end
 
       items.each do |item|
-        @log.debug "#{@mod_log} --- ENTRY ---"
+        mlog(:debug, "--- ENTRY ---")
         path = map_entry_paths(item)
         link_path = build_link_path(target_key, mod_name, path[:link_path])
         dest_path = build_dest_path(mod_name, path[:dest_path])
 
         unless File.exist?(dest_path)
-          @log.warn "#{@mod_log} Target does not exist: #{short_log(dest_path)}"
+          mlog(:warn, "Target does not exist: {path}", { path: dest_path })
           next
         end
 
@@ -130,21 +165,21 @@ class DotLinker
   def parse_yaml(mod_name)
     mod_conf = File.join(@dotfiles_dir, mod_name, MOD_FILE)
 
-    @log.info "#{@mod_log} Module conf: #{short_log(mod_conf)}"
-    @log.info "#{@mod_log} Parsing module file..."
+    mlog("Module path: {path}", { path: mod_conf })
+    mlog("Parsing module file...")
     config_data = YAML.load_file(mod_conf)
 
     unless config_data && config_data[MOD_INFO].is_a?(Hash)
-      @log.fatal "#{@mod_log} Invalid config structure, no '#{MOD_INFO}' section found!"
+      mlog(:fatal, "Invalid config structure, no '#{MOD_INFO}' section found!")
       return {}
     end
 
     config_data
   rescue Psych::SyntaxError => e
-    LoggerConfig.log_exception(@log, "#{@mod_log} Invalid YAML syntax!", e)
+    mlog("Invalid YAML syntax!", exception: e)
     {}
   rescue StandardError => e
-    LoggerConfig.log_exception(@log, "#{@mod_log} Could not load YAML!", e)
+    mlog("Could not load YAML!", exception: e)
     {}
   end
 
@@ -162,7 +197,7 @@ class DotLinker
     dest_p = paths.size > 1 ? paths[1] : paths[0] # If no colon, dest is same as link
 
     if link_p.nil? || link_p.empty?
-      @log.warn "#{@mod_log} Invalid item format, link part is empty: #{item}"
+      mlog(:warn, "Invalid item format, link part is empty: #{item}")
       return { link_path: nil, dest_path: nil }
     end
 
@@ -181,8 +216,8 @@ class DotLinker
     components = [base_path, *paths[1..], path]
     link_path = File.join(*components.compact)
 
-    @log.debug "#{@mod_log} Base: #{short_log(base_path)}"
-    @log.debug "#{@mod_log} Link: #{short_log(link_path)}"
+    mlog(:debug, "Base: {path}", { path: base_path })
+    mlog(:debug, "Link: {path}", { path: link_path })
     link_path
   end
 
@@ -190,7 +225,7 @@ class DotLinker
     components = [@dotfiles_dir, mod_name, path]
     dest_path = File.join(*components.compact)
 
-    @log.debug "#{@mod_log} Dest: #{short_log(dest_path)}"
+    mlog(:debug, "Dest: {path}", { path: dest_path })
     dest_path
   end
 
@@ -207,26 +242,23 @@ class DotLinker
     # REPO_SYMLINK_NAME = "dotfiles"
     # repo_symlink_full_path becomes ~/.config/dotfiles
     dotfiles_link = File.join(base_target_dir, REPO_SYMLINK_NAME)
-    link_msg = "{ #{short_log(dotfiles_link)} -> #{short_log(@dotfiles_dir)} }"
-
-    @log.info "#{@mod_log} Checking dotfiles link #{link_msg}"
+    mlog("Checking dotfiles link {{link} -> {target}}",
+         { link: dotfiles_link, target: @dotfiles_dir })
 
     if File.symlink?(dotfiles_link)
       if File.readlink(dotfiles_link) == @dotfiles_dir
-        @log.info "#{@mod_log} Dotfiles link found..."
+        mlog("Dotfiles link found...")
         return true
       end
 
-      @log.warn "#{@mod_log} Found dotfiles link to #{dotfiles_link}"
+      mlog(:warn, "Found dotfiles link to {path}", path: dotfiles_link)
       FileUtils.rm(dotfiles_link, noop: @dry_run, verbose: @verbose)
     elsif File.exist?(dotfiles_link)
-      @log.error "#{@mod_log} #{short_log(dotfiles_link)} exists and is not a symlink!"
-      @log.error "#{@mod_log} Please remove it manually or resolve this conflict"
+      mlog(:error, "{path} exists and is not a symlink!", { path: dotfiles_link })
+      mlog(:error, "Please remove it manually or resolve this conflict")
 
       raise "Conflict with with dotfiles link path #{dotfiles_link}"
     end
-
-    @log.info "#{@mod_log} Creating dotfiles link #{link_msg}"
 
     parent_dir = File.dirname(dotfiles_link)
     FileUtils.mkdir_p(parent_dir, noop: @dry_run, verbose: @verbose) unless Dir.exist?(parent_dir)
@@ -234,7 +266,8 @@ class DotLinker
 
     true
   rescue SystemCallError => e
-    LoggerConfig.log_exception(@log, "#{@mod_log} Error managing dotfiles link #{link_msg}!", e)
+    mlog("Error managing dotfiles link {{link} -> {target}}!",
+         { link: dotfiles_link, target: @dotfiles_dir, exception: e })
     raise
   end
 
@@ -245,17 +278,17 @@ class DotLinker
     if ops_flag[:backup]
       backup(dest_path, link_path)
     elsif ops_flag[:remove_first]
-      @log.info "#{@mod_log} Removing existing link: #{short_log(link_path)}"
+      mlog("Removing existing link: {path}", { path: link_path })
       FileUtils.rm(link_path, noop: @dry_run, verbose: @verbose)
     end
 
-    @log.info "#{@mod_log} Creating link { #{short_log(dest_path)} -> #{short_log(link_path)} }"
+    mlog("Creating link {{dest} -> {link}}", dest: dest_path, link: link_path)
     FileUtils.ln_s(dest_path, link_path, force: ops_flag[:force], noop: @dry_run, verbose: @verbose)
   rescue Errno::ENOENT => e
-    LoggerConfig.log_exception(@log, "#{@mod_log} Error on file operations!", e)
+    mlog("Error on file operations!", exception: e)
     false
   rescue StandardError => e
-    LoggerConfig.log_exception(@log, "#{@mod_log} Unexpected error!", e)
+    mlog("Unexpected error!", exception: e)
     false
   end
 
@@ -269,13 +302,13 @@ class DotLinker
     if !File.exist?(link_path) && !File.symlink?(link_path)
       # Case 1: Link path does not exist at all
       # All ops remain false, just proceed to link.
-      @log.info "#{@mod_log} No existing item at #{short_log(link_path)}"
+      mlog("No existing item at {path}", { path: link_path })
     elsif File.symlink?(link_path)
       # Case 2: Link path is an existing symlink
       check_symlink(dest_path, link_path, ops)
     elsif File.exist?(link_path)
       # Case 3: Link path is an existing file or directory (but not a symlink)
-      @log.info "#{@mod_log} Existing file/directory found at #{short_log(link_path)}"
+      mlog("Existing file/directory found at {path}", { path: link_path })
       ops[:backup] = true
     end
 
@@ -287,27 +320,27 @@ class DotLinker
 
     if !File.exist?(link_path)
       # Case 2a: Broken symlink
-      @log.info "#{@mod_log} Found broken link: #{short_log(link_path)}"
+      mlog("Found broken link: {path}", path: link_path)
       ops[:force] = true # Force will overwrite the broken symlink
     elsif File.directory?(link_path_actual) && File.directory?(dest_path)
-      @log.info "#{@mod_log} Found existing link: #{short_log(link_path)}"
+      mlog("Removing existing link: {path}", { path: link_path })
 
       if link_path_actual == dest_path
         # Case 2b: Existing symlink already points to the correct dest_path.
         # Flag as skip and continue processing other entries.
-        @log.info "#{@mod_log} Target is already correct, skipping this entry"
+        mlog("Target is already correct, skipping this entry")
         ops[:skip] = true
       else
         # Case 2c: Existing symlink points to a directory, and new target is also a directory.
-        # This aims for 'ln -sfn' behavior: remove old symlink, then create new one.
-        @log.info "#{@mod_log} Target is a directory, preparing to re-link"
+        # This aims for \'ln -sfn\' behavior: remove old symlink, then create new one.
+        mlog("Target is a directory, preparing to re-link")
         ops[:remove_first] = true
       end
     else
       # Case 2d: Symlink exists, is not broken, and not the dir-to-dir case above.
       # This includes symlink-to-file, symlink-to-non_dir_target etc.
-      @log.info "#{@mod_log} Found existing link: #{short_log(link_path)}"
-      @log.info "#{@mod_log} Link will be overwritten by force!"
+      mlog("Found existing link: {path}", { path: link_path })
+      mlog("Link will be overwritten by force!")
       ops[:force] = true
     end
   end
@@ -319,7 +352,7 @@ class DotLinker
     bak_name = "#{File.basename(dest_path)}_backup_#{Time.now.strftime('%F_%T')}"
     bak_path = File.join(bak_root, bak_name)
 
-    @log.info "#{@mod_log} Creating backup: #{short_log(bak_path)}"
+    mlog("Creating backup: {path}", { path: bak_path })
     FileUtils.move(link_path, bak_path, noop: @dry_run, verbose: @verbose)
   end
 
